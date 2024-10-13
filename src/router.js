bd84057c7214e02638d1fe7b25518e966cc355ee
rkgupta-dev <rohitkrgupta333@gmail.com>
2024-10-13 16:44:05 +0530

    updates

--- src/router.js
+++ src/router.js
@@ -43,4 +43,13 @@ export default new Router({
       component: BlogDetails03,
     },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    // If there is a saved scroll position, return it (for navigating back)
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      // If not, scroll to the top
+      return { x: 0, y: 0 };
+    }
+  },
 });
